9b21e14ec557b7ed342ef2a07576a7a5d1e0da49
Joshua  Dadson <joshuadadson@Joshuas-MacBook-Air.local>
2026-01-01 15:29:33 -0600

    mui table

--- app/_components/bluechip-table.tsx
+++ app/_components/bluechip-table.tsx
@@ -4,6 +4,13 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { convertToCurrency } from "@/utils";
 import { useRouter } from "next/navigation";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export function BluechipTable() {
     const [coins, setCoins] = useState([])
@@ -30,34 +37,36 @@ export function BluechipTable() {
 
     const renderTableRows = () => {
         return coins.map((coin, index) =>
-            <tr key={index} className="border-b-1 border-gray-400 h-15 hover:bg-blue-400 cursor-pointer" onClick={() => handleRowClick(coin)}>
-                <td className="w-5">{coin['market_cap_rank']}</td>
-                <td className="h-15 flex flex-row justify-left items-center font-bold">
+            <TableRow key={index} className="hover:bg-blue-400 cursor-pointer" onClick={() => handleRowClick(coin)}>
+                <TableCell className="w-10">{coin['market_cap_rank']}</TableCell>
+                <TableCell className="h-15 flex flex-row justify-left items-center font-bold">
 
-                    <Image className="scale-50" src={coin['image']} alt="Coin Image" width={50} height={20} />
+                    <Image className="mr-2" src={coin['image']} alt="Coin Image" width={25} height={10} />
                     {coin['name']}
-                </td>
-                <td>{convertToCurrency(coin['current_price'])}</td>
-                <td>{convertToCurrency(coin['market_cap'], 0)}</td>
-            </tr>
+                </TableCell>
+                <TableCell>{convertToCurrency(coin['current_price'])}</TableCell>
+                <TableCell>{convertToCurrency(coin['market_cap'], 0)}</TableCell>
+            </TableRow>
         )
     }
 
 
     return (
-        <table className="w-full">
-            <thead className="text-left">
-                <tr className="border-b-1">
-                    <th className="w-10">#</th>
-                    <th className="w-20">Coin</th>
-                    <th className="w-20">Price</th>
-                    <th className="w-20">Marketcap</th>
-                </tr>
-            </thead>
-            <tbody className="text-left">
-                {renderTableRows()}
-            </tbody>
-        </table>
+        <TableContainer>
+            <Table className="w-full">
+                <TableHead className="text-left">
+                    <TableRow>
+                        <TableCell className="w-10 font-bold">#</TableCell>
+                        <TableCell className="w-20 font-bold">Coin</TableCell>
+                        <TableCell className="w-20 font-bold">Price</TableCell>
+                        <TableCell className="w-20 font-bold">Market Cap</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody className="text-left">
+                    {renderTableRows()}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link"
 import Image from "next/image"
+import { AppBar, Toolbar } from "@mui/material"
 
 export default function Navbar() {
     return (
-        <nav className="flex flex-row items-center justify-between w-full h-15 bg-foreground text-background font-bold">
-            <div className="flex flex-row items-center justify-evenly w-full h-full">
-      {/* <div className="absolute left-0 text-5xl pl-3 text-blue-300">JD</div> */}            
-            <Link href="/">Home</Link>           
-            <Link href="/portfolio">Portfolio</Link> 
-            </div>      
-            <Image style={{fill: 'red'}} src="/user.svg" height={25} width={25} alt="Profile" className="cursor-pointer mr-5 hover:scale-120"/>
-        </nav>
+        <AppBar position="sticky" className="font-bold">
+            <Toolbar>
+                <div className="flex flex-row items-center justify-evenly w-full h-full">
+                    <Link href="/">Market</Link>
+                    <Link href="/portfolio">Portfolio</Link>
+                </div>
+                <Image style={{ fill: 'red' }} src="/user.svg" height={25} width={25} alt="Profile" className="cursor-pointer mr-5 hover:scale-120" />
+            </Toolbar>
+        </AppBar>
     )
 }
